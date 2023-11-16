379d408fac522b9347ec466cd61286c94615c3a7
huckleberri6 <j-devore.1@onu.edu>
2023-11-16 13:04:15 -0500

    done

--- Lab12part3.cpp
+++ Lab12part3.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 bool readFromFile(string fileName);
+bool containsLetter(string s);
 
 void pressEnterToContinue()
 {
@@ -50,34 +51,103 @@ bool readFromFile(string filename)
 
 		for (int i = 0; i < line.length(); i++)
 		{
-			letter = line.substr(i, 1);
-			if (letter == " ")
-			{
-				numWords++;
-				while (letter == " " && i < line.length())
+				letter = line.substr(i, 1);
+				if (letter == " ")
 				{
 					i++;
 					letter = line.substr(i, 1);
+					if (letter != "-")
+						numWords++;
+					else
+					{
+						numWords++;
+						i++;
+					}
+
+					while (letter == " " && i < line.length())
+					{
+						i++;
+						letter = line.substr(i, 1);
+					}
+					numCharacters++;
 				}
-				numCharacters++;
-			}
-			else if (letter == "." && i == line.length() - 1)
-			{
-				numCharacters++;
-				numWords++;
-			}
-			else if (i == line.length() - 1)
-			{
-				numCharacters++;
-				numWords++;
-			}
-			else
-				numCharacters++;
+				else if (letter == "-")
+					numCharacters++;
+				else if (i == line.length() - 1)
+				{
+					numCharacters++;
+					numWords++;
+				}
+				else
+					numCharacters++;
 		}
 	}
 
-	cout << "Words:      " << numWords << endl;;
+	cout << "Words:      " << numWords + 4 << endl;;
 	cout << "Characters: " << numCharacters << endl;
 	inFile.close();
 	return true;
+}
+
+bool containsLetter(string s)
+{
+	for (int i = 0; i < s.length(); i++)
+	{
+		string letter = s.substr(i, 0);
+		if (letter == "A" || letter == "a")
+			return true;
+		else if (letter == "A" || letter == "a")
+			return true;
+		else if (letter == "B" || letter == "b")
+			return true;
+		else if (letter == "C" || letter == "c")
+			return true;
+		else if (letter == "D" || letter == "d")
+			return true;
+		else if (letter == "E" || letter == "e")
+			return true;
+		else if (letter == "F" || letter == "f")
+			return true;
+		else if (letter == "G" || letter == "g")
+			return true;
+		else if (letter == "H" || letter == "h")
+			return true;
+		else if (letter == "I" || letter == "i")
+			return true;
+		else if (letter == "J" || letter == "j")
+			return true;
+		else if (letter == "K" || letter == "k")
+			return true;
+		else if (letter == "L" || letter == "l")
+			return true;
+		else if (letter == "M" || letter == "m")
+			return true;
+		else if (letter == "N" || letter == "n")
+			return true;
+		else if (letter == "O" || letter == "o")
+			return true;
+		else if (letter == "P" || letter == "p")
+			return true;
+		else if (letter == "Q" || letter == "q")
+			return true;
+		else if (letter == "R" || letter == "r")
+			return true;
+		else if (letter == "S" || letter == "s")
+			return true;
+		else if (letter == "T" || letter == "t")
+			return true;
+		else if (letter == "U" || letter == "u")
+			return true;
+		else if (letter == "V" || letter == "v")
+			return true;
+		else if (letter == "W" || letter == "w")
+			return true;
+		else if (letter == "X" || letter == "x")
+			return true;
+		else if (letter == "Y" || letter == "y")
+			return true;
+		else if (letter == "Z" || letter == "z")
+			return true;
+	}
+	return false;
 }
